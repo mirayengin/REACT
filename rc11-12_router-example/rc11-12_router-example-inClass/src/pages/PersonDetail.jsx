33931cf770c0562fb918829fc92db2d81e3a0f80
erhan <erhanserkansabriye@gmail.com>
2022-10-24 22:49:19 +0300

    router dersi iki yarım kaldı

--- rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
 
 const PersonDetail = () => {
   const navigate = useNavigate();
@@ -14,33 +15,62 @@ const PersonDetail = () => {
   // console.log(person);
 
   const [person, setPerson] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`https://reqres.in/api/users/${id}`)
-      .then((res) => res.json())
-      .then((data) => setPerson(data.data))
+      .then((res) => {
+        if (!res.ok) {
+          setError(true);
+          setLoading(false);
+          throw new Error("Something went wrong");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setLoading(false);
+        setPerson(data.data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
   console.log(person);
 
-  return (
-    <div className="container text-center">
-      <h3>
-        {person?.first_name} {person?.last_name}
-      </h3>
-      <img className="rounded" src={person?.avatar} alt="" />
-      <p>{person?.email}</p>
-      <div>
-        <button onClick={() => navigate("/")} className="btn btn-success me-2">
-          Go Home
-        </button>
-        <button onClick={() => navigate(-1)} className="btn btn-warning">
-          Go Back
-        </button>
+  if (error) {
+    return <NotFound />;
+  }
+
+  if (loading) {
+    return (
+      <div className="text-center">
+        <h3>Data Loading</h3>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (!error && !loading) {
+    return (
+      <div className="container text-center">
+        <h3>
+          {person?.first_name} {person?.last_name}
+        </h3>
+        <img className="rounded" src={person?.avatar} alt="" />
+        <p>{person?.email}</p>
+        <div>
+          <button
+            onClick={() => navigate("/")}
+            className="btn btn-success me-2"
+          >
+            Go Home
+          </button>
+          <button onClick={() => navigate(-1)} className="btn btn-warning">
+            Go Back
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default PersonDetail;
