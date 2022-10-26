433253515314452b2dfefb0418003e82bc8b4cf1
erhan <erhanserkansabriye@gmail.com>
2022-10-26 23:38:51 +0300

    styled son ders  tekarrı kaldı

---
+++ rc13_styled-comps-example/styled-example-starter/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../styles/Button.Styled'
+
+const Header = () => {
+  return (
+    <div>
+      <Button color="#A62440">Apply Courses</Button>
+      <Button bg="#A62440">Talk To Adviser</Button>
+    </div>
+  )
+}
+
+export default Header
