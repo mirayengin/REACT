817d96cf1e23ec0d9ff11d3763927fb4a8c7c849
erhan <erhanserkansabriye@gmail.com>
2022-10-27 13:48:35 +0300

    styled component dersi tekrarı bitti

--- rc13_styled-comps-example/styled-example-starter/src/App.js
+++ rc13_styled-comps-example/styled-example-starter/src/App.js
@@ -1,10 +1,24 @@
 import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+
+
+//? ThemeProvider ı kullanmak için önce kullanacağımız componenti sarmallıyoruz bununla ve sonra oluşturduğumuz componenti (style) gönderiyoruz.
+const style = {
+  colors: {
+    header: "#fff",
+    body: "#fff",
+    footer: "#8A1C4A",
+  },
+
+  margins: {},
+  responsive: "768px",
+};
 
 const App = () => {
   return (
-    <>
-     <Header/>
-    </>
+    <ThemeProvider theme={style}>
+      <Header />
+    </ThemeProvider>
   );
 };
 
--- rc13_styled-comps-example/styled-example-starter/src/components/Header.jsx
+++ rc13_styled-comps-example/styled-example-starter/src/components/Header.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Button from '../styles/Button.Styled'
+import React from "react";
+import Button from "../styles/Button.Styled";
+import Flex from "../styles/Flex.Styled";
+import StyledHeader, { Image, Logo, Nav } from "../styles/Header.Styled";
 
 const Header = () => {
   return (
-    <div>
-      <Button color="#A62440">Apply Courses</Button>
-      <Button bg="#A62440">Talk To Adviser</Button>
-    </div>
-  )
-}
+    <StyledHeader>
+      <Nav>
+        <Logo src="./images/logo.png" />
+        <div>
+          <Button color="#A62440">Apply Courses</Button>
+          <Button bg="#A62440">Talk To Adviser</Button>
+        </div>
+      </Nav>
 
-export default Header
+      <Flex>
+        <div>
+          <h1>Lorem ipsum dolor sit amet.</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi hic
+            veritatis quae ducimus sed aliquid dolores quo iste nostrum non!
+          </p>
+          <Button bg="#A62440">Start Your New Carier</Button>
+        </div>
+
+        <Image src="./images/hero.jpg"/>
+      </Flex>
+    </StyledHeader>
+  );
+};
+
+export default Header;
