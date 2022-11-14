e894abc53c6388aa33916d9c3f0eff5b082428a6
erhan <erhanserkansabriye@gmail.com>
2022-11-14 21:41:12 +0300

    ilkders

---
+++ rc23_Fire-contact/fire_contact_project_solution/src/utils/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqd5VoOqeRghE4pTIP1_hd2mnVk4sOx_U",
+  authDomain: "firecontact-d32d8.firebaseapp.com",
+  databaseURL:
+    "https://firecontact-d32d8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "firecontact-d32d8",
+  storageBucket: "firecontact-d32d8.appspot.com",
+  messagingSenderId: "1080601116352",
+  appId: "1:1080601116352:web:38eb3df0f3628cd5a7f725",
+};
+
+const app = initializeApp(firebaseConfig);
