6f577697695dc5be9f3196b7b655315a540a36c4
erhan <erhanserkansabriye@gmail.com>
2022-11-22 22:19:17 +0300

    ders son

--- rc25_stock-app/stock-app/src/pages/Firms.jsx
+++ rc25_stock-app/stock-app/src/pages/Firms.jsx
@@ -1,4 +1,38 @@
+import { useEffect } from "react";
+import useStockCalls from "../hooks/useStockCalls";
+// import axios from "axios";
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+
 const Firms = () => {
+  const { getFirms, getSales } = useStockCalls();
+
+  // const dispatch = useDispatch();
+
+  // const { token } = useSelector((state) => state.auth);
+  // const BASE_URL = "https://10001.fullstack.clarusway.com/";
+
+  // const getFirms = async () => {
+  //   const url = "firms";
+  //   dispatch(fetchStart());
+  //   try {
+  //     const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     });
+  //     console.log(data);
+  //     dispatch(getSuccess({ data, url }));
+  //   } catch (error) {
+  //     dispatch(fetchFail());
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    getFirms();
+    getSales();
+  }, []);
+
   return <div>Firms</div>;
 };
 
